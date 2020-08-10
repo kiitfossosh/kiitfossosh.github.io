443ae627451cee5b390d2525e9ab8700230d335d
Junaid Rahim <junaidrahim8d@gmail.com>
2020-08-11 03:52:38 +0530

    clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `KIIT FOSS/OSH`,
+    description: `KIIT's Free and Open Source Software and Hardware Community.`,
+    author: `Junaid Rahim`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
